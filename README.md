17dea03709a441e1d95c1dcc0e1d5b4cbf842219
Veronica Yurek <31707041+vryurek@users.noreply.github.com>
2022-07-02 00:54:53 -0500

    connected to mongo and set up cookies

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DNDCharacterBuilder
+Used this tutorial as a base: https://samippoudel.hashnode.dev/google-oauth-using-typescript-expressjs-passportjs-and-mongodb-ckvatnioy0hsu45s1db0r3qoy
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,37 @@
 import express from "express";
 import mongoose from "mongoose";
-import { MONGO_URI, PORT } from "./utils/secrets";
+import { COOKIE_KEY, MONGO_URI,PORT } from "./utils/secrets";
 import authRoutes from "./routes/authRoutes";
 import "./config/passport";
-
-
+import cookieSession from "express-session";
+import passport from "passport";
 
 const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(
+  cookieSession({
+    secret: [COOKIE_KEY],
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect(MONGO_URI, () => {
   console.log("connected to mongodb");
 });
 
-app.listen(PORT, () => {
-  console.log("App listening on port: " + PORT);
-});
+app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.use("/auth", authRoutes);
+app.listen(PORT, () => {
+  console.log("App listening on port: " + PORT);
+});
